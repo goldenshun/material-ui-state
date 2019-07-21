d522a58da7ae2a94f0dec245ddfde5b7ada2610b
Sean Connolly <sconnolly17@gmail.com>
2019-07-20 21:56:02 -0600

    Wip snackbar options

--- pages/snackbar.js
+++ pages/snackbar.js
@@ -2,11 +2,15 @@ import { Button, Typography } from '@material-ui/core';
 import { useSnackbar } from '../components/snackbar-state';
 
 const SnackbarPage = () => {
-  const { showSnackbar } = useSnackbar();
+  const { showSnackbar } = useSnackbar({
+    options: {
+      onClose: () => console.log('Snackbar closed'),
+    },
+  });
 
   return (
     <>
-      <Typography variant="h1">Popper</Typography>
+      <Typography variant="h1">Snackbar</Typography>
       <div>
         <Button onClick={() => showSnackbar({ message: 'Hey' })}>
          Show Snackbar
